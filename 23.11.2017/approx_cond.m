b21808a059b3948e0b327fda4abcb00bd1123bc5
Ianis G. Vasilev <ianis@ivasilev.net>
2017-11-23 18:54:58 +0200

    Add exercises from 23.11.2017

---
+++ 23.11.2017/approx_cond.m
@@ -0,0 +1,17 @@
+function[condition, approx] = approx_cond(n)
+    % A is a symmetric tridiagonal matrix
+    A = diag(2 * ones(n, 1)) + diag(-ones(n - 1, 1), 1) + diag(-ones(n - 1, 1), -1);
+
+    condition = cond(A, 2);
+    approx = (4 / pi^2) * (n + 1)^2;
+end
+
+%!test
+%! source support/assert_convergence.m
+%! source support/benchmark.m;
+%! source 23.11.2017/approx_cond.m
+%!
+%! [conds, approx] = benchmark_scalar(10:10:200, @approx_cond);
+%!
+%! % Verify that the ratio converges to 1
+%! assert_fundamental(conds ./ approx - 1);
--- tests.m
+++ tests.m
@@ -10,3 +10,5 @@ test 09.11.2017/cholesky_solve
 
 test 16.11.2017/gauss_tridiagonal
 test 16.11.2017/benchmark_gauss_tridiagonal
+
+test 23.11.2017/approx_cond
