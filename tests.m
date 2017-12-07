0d96524139687a8770431e59517d261af9c5a3c3
Ianis G. Vasilev <ianis@ivasilev.net>
2017-12-07 23:26:16 +0200

    Add semicolons to the tests file

--- tests.m
+++ tests.m
@@ -1,14 +1,14 @@
-test 12.10.2017/gauss
+test 12.10.2017/gauss;
 
-test 19.10.2017/gauss_with_pivoting
-test 19.10.2017/lu
-test 19.10.2017/lu_solve
+test 19.10.2017/gauss_with_pivoting;
+test 19.10.2017/lu;
+test 19.10.2017/lu_solve;
 
-test 09.11.2017/benchmark_gauss
-test 09.11.2017/cholesky
-test 09.11.2017/cholesky_solve
+test 09.11.2017/benchmark_gauss;
+test 09.11.2017/cholesky;
+test 09.11.2017/cholesky_solve;
 
-test 16.11.2017/gauss_tridiagonal
-test 16.11.2017/benchmark_gauss_tridiagonal
+test 16.11.2017/gauss_tridiagonal;
+test 16.11.2017/benchmark_gauss_tridiagonal;
 
-test 23.11.2017/approx_cond
+test 23.11.2017/approx_cond;
