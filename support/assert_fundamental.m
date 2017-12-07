d9f2eaee4431a6539a113240c8950d852864d69f
Ianis G. Vasilev <ianis@ivasilev.net>
2017-12-07 21:08:34 +0200

    Rename the file containing the assert_fundamental helper

--- support/assert_fundamental.m
+++ support/assert_fundamental.m
@@ -1,5 +1,5 @@
 function[result] = assert_fundamental(vector)
     n = size(vector, 2);
     diff = abs(vector(1, 2:n) - vector(1, 1:n - 1));
-    assert(diff(2:n - 1) < diff(1:n - 2), sprintf('Not a fundamental sequence'))
+    assert(diff(2:n - 1) < diff(1:n - 2), 'Not a fundamental sequence');
 end
